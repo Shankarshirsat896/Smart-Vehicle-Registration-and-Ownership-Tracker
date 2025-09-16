bf5c4959ad1cbd98972a205ea82cc339517337f7
Shankar Shirsat <123562202+Shankarshirsat896@users.noreply.github.com>
2025-09-16 18:39:13 +0530

    Initial commit

---
+++ src/main/java/com/VROMS/SmartVehicleRegistrationAndOwnershipTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.VROMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartVehicleRegistrationAndOwnershipTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartVehicleRegistrationAndOwnershipTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VROMS/SmartVehicleRegistrationAndOwnershipTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VROMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartVehicleRegistrationAndOwnershipTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
