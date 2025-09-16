1473ad8ad4d71bdea0b18620df2aa7fa1246139c
Ajay Mali <168232654+AjayMali1122@users.noreply.github.com>
2025-09-16 21:07:09 +0530

    Update SmartVehicleRegistrationAndOwnershipTrackerApplication.java

--- src/main/java/com/SVROT/SmartVehicleRegistrationAndOwnershipTrackerApplication.java
+++ src/main/java/com/SVROT/SmartVehicleRegistrationAndOwnershipTrackerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartVehicleRegistrationAndOwnershipTrackerApplication {
 
 	public static void main(String[] args) {
+				System.out.println("Hello");
+
 		SpringApplication.run(SmartVehicleRegistrationAndOwnershipTrackerApplication.class, args);
 	}
 
