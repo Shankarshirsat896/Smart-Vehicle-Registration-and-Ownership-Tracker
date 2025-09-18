9970f1d289531048a505de29fe6aa117fea6a902
AjayMali1122 <maliajay9728@gmail.com>
2025-09-18 20:11:05 +0530

    checking code

--- src/main/java/com/SVROT/SmartVehicleRegistrationAndOwnershipTrackerApplication.java
+++ src/main/java/com/SVROT/SmartVehicleRegistrationAndOwnershipTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.VROMS;
+package com.SVROT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartVehicleRegistrationAndOwnershipTrackerApplication {
 
 	public static void main(String[] args) {
-        System.out.println("Ajay");
-    main
+
 		SpringApplication.run(SmartVehicleRegistrationAndOwnershipTrackerApplication.class, args);
 
 	}
